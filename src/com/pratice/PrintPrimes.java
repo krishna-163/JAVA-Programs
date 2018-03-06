d6b167995cbcbef764d4bc088e85b8f97cbd9c38
Vamsi Krishna <krishna.yalagala@gmail.com>
2018-03-06 12:14:24 -0600

    commit

--- src/com/pratice/PrintPrimes.java
+++ src/com/pratice/PrintPrimes.java
@@ -3,17 +3,27 @@ package com.pratice;
 public class PrintPrimes {
 	public void printPrimenumbers() {
 		CheckPrime ch = new CheckPrime();
-		for(int i=3; i<=150; i++) {
+		for (int i = 3; i <= 150; i++) {
 			boolean stas = ch.isPrime(i);
-			System.out.println(i+" is "+ stas);
-			
+			System.out.println(i + " is " + stas);
+
 		}
-		
+
+		/*
+		 * 
+		 * public class PrintPrimes { .. extend CheckPrime
+		 * public void printPrimenumbers() {    .. insted of calling objest we can add 
+		 *  CheckPrime ch = new CheckPrime();
+		 *   for(int i=3; i<=150; i++) { 
+		 *   boolean stas = ch.isPrime(i);
+		 * System.out.println(i+" is "+ stas); *
+		 */
+
 	}
 
 	public static void main(String[] args) {
-		
-		PrintPrimes p=new PrintPrimes();
+
+		PrintPrimes p = new PrintPrimes();
 		p.printPrimenumbers();
 		// TODO Auto-generated method stub
 
---
+++ src/com/pratice/Scann.java
@@ -0,0 +1,23 @@
+package com.pratice;
+
+import java.util.Scanner;
+
+public class Scann {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number");
+		int x;
+		x = sc.nextInt();
+		System.out.println("Number is " + x);
+
+		Scanner pc = new Scanner(System.in);
+		System.out.println("Enter Word");
+		String y;
+		y = pc.nextLine();
+		System.out.println("Word is " + y);
+	}
+
+}
