312dcc7a492b548520fa8809944452739fa505d2
Vamsi Krishna <krishna.yalagala@gmail.com>
2018-03-08 15:14:43 -0600

    add

--- src/com/pratice/Array.java
+++ src/com/pratice/Array.java
@@ -14,6 +14,8 @@ public class Array {
 	
 		
 		for(i=0;i<array.length;i++)
+			
+			/// hvckjkvkglkglkglkjbnvmkbm,
 			{
 			System.out.print(array[i]+" ");
 			}
---
+++ src/com/pratice/Slkl.java
@@ -0,0 +1,5 @@
+package com.pratice;
+
+public class Slkl {
+
+}
